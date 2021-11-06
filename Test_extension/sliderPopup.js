1d15a1dcca9bc038fb0a9acadf46966570ae9367
Aidan McHugh <mchughaw@gmail.com>
2021-11-06 15:09:41 -0400

    new js + html for extension

--- Test_extension/sliderPopup.js
+++ Test_extension/sliderPopup.js
@@ -1,8 +1,20 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+// // Update the current slider value (each time you drag the slider handle)
+// slider.oninput = function() {
+  // output.innerHTML = this.value;
+// }
+
+function foo() {
+  var output = $('#demo')
+  $('#bias').on('input', function() {
+    $('#bias-value').text(this.value);
+  })
+  $('#reliability').on('input', function() {
+    $('#reliability-value').text(this.value);
+  })
 }
+
+$(foo)
